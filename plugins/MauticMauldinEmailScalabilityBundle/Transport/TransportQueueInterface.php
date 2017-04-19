21dbe208aeda19a41a907f4081fe2bb7fb83fc28
Max Lawton <max@mauldineconomics.com>
2017-04-19 14:30:01 -0400

    Define QueuedTransportInterface for execution test
    The Brick Abode core changes rely on testing if a mailer transport
    object has a property named 'rabbitmqidproperty'. That property is
    defined on our RabbitmqTransport for the sake of this test, but is
    otherwise unused. In modern PHP, it is more common to test whether an
    object implements an interface that provides the [un]desired
    functionality. For this purpose, such an interface has been defined in
    the QueuedTransportInterface. That interface defines a setter
    injection method, 'setTransportQueue', which takes an instance of
    TransportQueueInterface as its sole argument. There's also a getter
    which will throw an exception if the queue property is not bound. The
    queue's interface has also been defined, and implementation thereof
    will be used to provide our transport with access to the message queue
    functionality.

---
+++ plugins/MauticMauldinEmailScalabilityBundle/Transport/TransportQueueInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * @package   Mauldin Email Scalability
+ * @copyright Copyright(c) 2017 by GGC PUBLISHING, LLC
+ * @author    Max Lawton <max@mauldineconomics.com>
+ */
+
+namespace MauticMauldinEmailScalabilityBundle\Transport;
+
+/**
+ * Transport Queue Interface.
+ */
+interface TransportQueueInterface
+{
+    /**
+     * Publish a message to the underlying queue implementation.
+     *
+     * @param mixed $message
+     */
+    public function publish($message);
+
+    /**
+     * Consume $callback on the underlying message queue.
+     *
+     * @param \Callable $callback
+     */
+    public function consume($callback);
+}
