a8a279ab135329ffea177a38793387a2d9fc582b
Max Lawton <max@mauldineconomics.com>
2017-04-19 14:43:54 -0400

    Override modified core services via compiler pass
    The initial implementation of this plugin by Brick Abode relied on
    changes to the core MailHelper and EmailModel classes, wherein the
    mailer transport is checked for use of an AMQP message queue.
    
    In the interest of avoiding direct modification of core classes, this
    bundle introduces two new classes which extend their respective core
    classes and define method overrides as appropriate. A CompilerPass has
    been added to the bundle load method which replaces the instantiated
    class with our extended implementation for each of the relevant
    application services.
    
    The overridden methods--as now provided by the plugin--differ from
    earlier core alterations in that the mailer transport is now tested
    for implementation of the QueuedTransportInterface rather than the
    existence of an otherwise unused class property.

--- plugins/MauticMauldinEmailScalabilityBundle/MauticMauldinEmailScalabilityBundle.php
+++ plugins/MauticMauldinEmailScalabilityBundle/MauticMauldinEmailScalabilityBundle.php
@@ -4,12 +4,26 @@
  * @package     Mauldin Email Scalability
  * @copyright   Copyright(c) 2017 by GGC PUBLISHING, LLC
  * @author      Brick Abode
+ * @author      Max Lawton <max@mauldineconomics.com>
  */
 
 namespace MauticPlugin\MauticMauldinEmailScalabilityBundle;
 
 use Mautic\PluginBundle\Bundle\PluginBundleBase;
+use MauticPlugin\MauticMauldinEmailScalabilityBundle\DependencyInjection\Compiler\QueuedTransportPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class MauticMauldinEmailScalabilityBundle extends PluginBundleBase
 {
+    /**
+     * Build.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new QueuedTransportPass());
+    }
 }
